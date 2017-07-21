a4142260fec589b591e9bfcb637492c875c5a010
MdL <mdlmdel@gmail.com>
2017-07-21 18:21:02 -0400

    Updated frontend

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,18 @@
+// NOTE: SOMETHING IS WRONG WITH THE STORE. WHEN I UN-COMMENT OUT THE BELOW, THE ERRORS APPEAR
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { Provider } from 'react-redux';
+// Since we used export default, remove the curly brackets
+import store from './redux/store';
 
-ReactDOM.render(
-  <App />,
+
+//import './css/index.css';
+import App from './components/App';
+
+// import Routes from './Routes';
+
+ReactDOM.render(<Provider store={store}>
+  <App />
+  </Provider>,
   document.getElementById('root')
 );
